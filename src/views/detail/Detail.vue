4512625641894349df10b2865037625cd7018a1d
codingbylch <ilinf@foxmail.com>
2020-06-06 16:34:18 +0800

    店铺信息的解析与展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,26 +2,30 @@
   <div class="detail">
     <detail-nav-bar></detail-nav-bar>
     <detail-swiper :top-images="topImages"></detail-swiper>
+    <detail-base-info :goods="goods"></detail-base-info>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from "./childComps/DetailSwiper";
+import DetailBaseInfo from './childComps/DetailBaseInfo';
 
-import { getDetail } from "network/detail.js";
+import { getDetail, GoodsInfo } from "network/detail.js";
 
 export default {
   name: "Detail",
   data() {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {}
     };
   },
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo
   },
   created() {
     // 1.保存传入的id
@@ -29,8 +33,16 @@ export default {
     // 2.根据id 请求数据
     getDetail(this.iid).then(res => {
       console.log(res);
+      const data = res.result;
       // 1.获取顶部图片的轮播数据
-      this.topImages = res.result.itemInfo.topImages;
+      this.topImages = data.itemInfo.topImages;
+      // 2.获取商品信息
+      this.goods = new GoodsInfo(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.service
+      );
+      console.log(this.goods);
     });
   }
 };
---
+++ src/views/detail/childComps/DetailBaseInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      商品基本信息
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    goods: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
